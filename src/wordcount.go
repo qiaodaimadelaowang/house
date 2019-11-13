527e3791dfcf8e8ac9a462e69128c6d498077f66
tinyhui.wang <tinyhui.wang@gmail.com>
2019-11-13 09:36:00 +0800

    Exercise: Maps -> WordCount

---
+++ src/wordcount.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	var m map[string]int
+	m = make(map[string]int)
+	fields := strings.Fields(s)
+	for _, v := range fields {
+		if _, ok := m[v]; ok {
+			m[v] += m[v]
+		} else {
+			m[v] = 1
+		}
+	}
+	return m
+}
+
+func main() {
+	count := WordCount("sdf a d a d d ads wo owo wo")
+	for k, v := range count {
+		fmt.Printf("%s,%d\n", k, v)
+	}
+}
